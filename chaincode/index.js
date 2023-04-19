2c6ef24ecb05d524e2c413cc2324b190d6e73b6e
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-04-19 14:34:28 +0800

    Add PropertyRegistrationUserContract

---
+++ chaincode/index.js
@@ -0,0 +1,6 @@
+'use strict';
+//Module for Users Organization
+const PropertyRegistrationUserContract = require('./user-contract.js');
+
+//Export the contracts
+module.exports.contracts = [PropertyRegistrationUserContract];
